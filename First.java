fa3f12f5f9e9da3a064add8229ba0cf3c2a7954e
Mansi <mansisinght5@gmail.com>
2026-04-09 07:14:23 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,61 @@
+
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // int button = sc.nextInt();
+        // // int age = sc.nextInt();
+
+        // // if (age > 18){
+        // //     System.out.println("Adult");
+        // // } else {
+        // //     System.out.println("Not Adult");
+        // // }
+
+        // // int x = sc.nextInt();
+
+        // // if (x % 2 == 0) {
+        // //     System.out.println("EVEN");
+        // // } else {
+        // //     System.out.println("ODD");
+        // // }
+
+
+        // // int a = sc.nextInt();
+        // // int b = sc.nextInt();
+
+        // // if (a == b) {
+        // //     System.out.println("Equal");
+        // // } else {
+        // //     if (a > b) {
+        // //         System.out.println("a is greater than b");
+        // // } else {
+        // //         System.out.println("a is lesser than b");
+        // // }
+        // // }
+
+        // // int button = sc.nextInt();
+
+        // //  if(button == 1){
+        // //     System.out.println("Hello");
+        // //  } else if(button == 2) {
+        // //     System.out.println("Namaste");
+        // //  } else if (button == 3) {
+        // //     System.out.println("Bonjour");
+        // //  } else {
+        // //     System.out.println("Invalid button");
+        // //  }
+
+        // switch(button) {
+        //     case 1 : System.out.println("Hello");
+        //     break;
+        //     case 2 : System.out.println("Namaste");
+        //     break;
+        //     case 3 : System.out.println("Bonjour");
+        //     break;
+        //     default: System.out.println("Invalid Button");
+        // }
+    }
+    
+}
