fcf99da4e23da5d810e5297002d3aee1fd31aacd
Mansi <mansisinght5@gmail.com>
2026-04-10 10:22:21 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Loops {
+    public static void main(String[] args) {
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // // System.out.println("Hello world");
+
+        // for(int counter = 0; counter < 3; counter = counter + 1) {
+        //     System.err.println("Hello world");
+        // }
+
+        // //Counter ++ => counter = counter + 1
+        // for(int counter = 0; counter < 11; counter ++) {
+        //     System.out.println(counter);
+        // }
+        
+        // for(int counter = 0; counter < 11; counter++) {
+        //     System.out.print(counter+ " ");
+        // }
+
+        // for (int counter = 0; counter < 11; counter ++){
+        // //     System.out.println(counter);
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println(i);
+        // }
+
+        //  int i = 0 ;
+        //  while (i < 11) {
+        //     System.out.println(i);
+        //     i = i + 1 ; // i++ ;
+        //  }
+
+
+        // int i = 0;
+        // do { 
+        //     System.out.println(i);
+        //     i = i + 1;
+        // } while (i < 11);
+
+        //FALSE CONDITION
+
+        // int i = 12 ;
+        // while (i < 11) {
+        //     System.out.println("This is the time... actually high time");
+        // }
+
+        // do {
+        //     System.out.println("This is the time... actually high time");
+        // } while (i < 11);
+
+
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+
+        // int sum = 0;
+        // for (int i = 0; i <=n; i++){
+        //     sum = sum + i;
+        // }
+
+        // System.out.println(sum);
+         
+        for(int i=1; i<11; i ++) {
+            System.out.println(i*n);
+        }
+
+
+
+
+
+    }
+    
+}
---
+++ Patterns.java
@@ -0,0 +1,129 @@
+import java.util.*;
+
+public class Patterns {
+    public static void main(String[] args) {
+        // int n = 4;
+        // int m = 5;
+
+        //OUTER LOOP    
+        // for(int i=1; i<=n; i++){
+        //     //INNER LOOP
+        //     for (int j=1; j<=m; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // int n = 4;
+        // int m = 5;
+
+        // //outer loop
+        // for (int i=1; i<=n; i++) {
+        //     // INNER LOOP
+        //     for(int j=1; j<=m; j++) {
+        //         // cell -> (i,j)
+
+        //         if (i==1||j==1||i==n||j==m ){
+        //            System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //      System.out.println();
+        // }
+
+        // int n = 4;
+        // int m = 5;
+
+        // // OUTER LOOP
+
+        // for (int i=1; i<=n; i++){
+        //     // INNER LOOP
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 4;
+        // int m = 5;
+
+        // for(int i=n; i>=1; i--) {
+        //     for (int j = 1; j<= i ; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // int n = 4;
+         
+        // //OUTER LOOP
+        // for(int i=1; i<=n; i++) {
+        //     //INNER LOOP -> SPACE PRINT
+        //     for (int j = 1; j<=n-i; j++) {
+        //         System.out.print(" ");
+        //     }
+
+        //     //INNER LOOP -> STAR PRINT
+        //     for(int j=1; j<=i; j++)  {
+        //         System.out.print("*");
+        //     }
+
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+
+        // //OUTER LOOP
+        // for(int i = 1; i<=n; i++) {
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+
+        // // OUTER LOOP
+        // for(int i = 1; i<=n; i++) {
+        //     //INNER LOOP
+        //     for(int j=1; j<=n-i+1; j++) {
+        //         System.out.print(j);
+        //     }
+
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+        // int number = 1;
+
+        // //OUTER LOOP
+        // for(int i = 1; i<=n; i++) {
+        //     //INNER LOOP
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print(number + " ");
+        //         number++; //number = number + 1
+        //     }
+        //     System.out.println();
+        // }
+
+        //  int n = 5;
+
+        // //outer loop
+        // for(int i=1; i<=n; i++) {
+        //     //INNER LOOP
+        //     for(int j=1; j<=i; j++) {
+        //         int sum = i+j;
+        //         if(sum % 2 == 0) { //even
+        //             System.out.print("1 ");
+        //         } else { //odd
+        //             System.out.print("0 ");
+        //         } 
+        //     }
+        //     System.out.println();
+        // }
+
+    }
+}
